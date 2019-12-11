d14574501fad727c359a729dfa85e6718ef2c525
WJ <190565307@qq.com>
2019-12-11 16:01:48 +0800

    添加项目文件。

---
+++ Demo_WxTool/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Demo_WxTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDatToImage = new System.Windows.Forms.Button();
+            this.txtXor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDatToImage
+            // 
+            this.btnDatToImage.Image = ((System.Drawing.Image)(resources.GetObject("btnDatToImage.Image")));
+            this.btnDatToImage.Location = new System.Drawing.Point(185, 53);
+            this.btnDatToImage.Name = "btnDatToImage";
+            this.btnDatToImage.Size = new System.Drawing.Size(30, 23);
+            this.btnDatToImage.TabIndex = 0;
+            this.btnDatToImage.UseVisualStyleBackColor = true;
+            this.btnDatToImage.Click += new System.EventHandler(this.btnDatToImage_Click);
+            // 
+            // txtXor
+            // 
+            this.txtXor.Location = new System.Drawing.Point(62, 54);
+            this.txtXor.Name = "txtXor";
+            this.txtXor.Size = new System.Drawing.Size(110, 21);
+            this.txtXor.TabIndex = 1;
+            this.txtXor.Text = "6F";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "异或值";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 91);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(238, 10);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "文件夹";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(62, 18);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(189, 21);
+            this.txtFolder.TabIndex = 5;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.Location = new System.Drawing.Point(221, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(30, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 114);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtXor);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnDatToImage);
+            this.Name = "Form1";
+            this.Text = "微信Dat文件解码";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDatToImage;
+        private System.Windows.Forms.TextBox txtXor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Demo_WxTool/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_WxTool
+{
+    public partial class Form1 : Form
+    {
+        private bool IsRunning = false;
+        private bool IsStop = false;
+        private int successCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDatToImage_Click(object sender, EventArgs e)
+        {
+            if (IsRunning)
+            {
+                MessageBox.Show("程序正忙，请耐心等待..."); return;
+            }
+            try
+            {
+                var dir = new DirectoryInfo(txtFolder.Text);
+                if (dir == null) return;
+
+                successCount = 0;
+                var xorKey = Convert.ToInt64(txtXor.Text, 16);
+                var files = dir.GetFiles();
+                progressBar1.Value = 0;
+                progressBar1.Maximum = files.Length;
+                btnStop.Enabled = true;
+                btnDatToImage.Enabled = false;
+                IsRunning = true;
+                IsStop = false;
+                ThreadPool.SetMinThreads(1, 1);
+                ThreadPool.SetMaxThreads(5, 5);
+
+                //AutoResetEvent myEvent = new AutoResetEvent(false);
+                foreach (var file in files)
+                {
+                    if (IsStop) break;
+                    ThreadPool.QueueUserWorkItem(p => DatToImage(file, xorKey));
+                }
+                //myEvent.WaitOne();
+                //MessageBox.Show("线程池终止！");
+                //new Thread(new ThreadStart(delegate { DatToImage(files, txtXor.Text); })).Start();
+                //MessageBox.Show(string.Format("[{0}]已完成转换", successCount));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Reset();
+            }
+            finally
+            {
+            }
+        }
+        void DatToImage(FileInfo fileInfo, long key)
+        {
+            if (!IsStop)
+            {
+                byte[] data = null;
+                if (fileInfo.Extension.ToLower().Contains("dat"))
+                {
+                    using (var fs = new FileStream(fileInfo.FullName, FileMode.Open))
+                    {
+                        data = new byte[fs.Length];
+                        int count = fs.Read(data, 0, data.Length);
+                        var fileName = fileInfo.FullName + ".jpg";
+                        using (var nfs = new FileStream(fileName, FileMode.OpenOrCreate))
+                        {
+                            foreach (var b in data)
+                            {
+                                nfs.WriteByte((byte)(b ^ key));
+                                nfs.Flush();
+                            }
+                        }
+                        Console.WriteLine("正在处理：" + fileInfo.FullName);
+                    }
+                }
+            }
+            Interlocked.Increment(ref successCount);
+            progressBar1.BeginInvoke((MethodInvoker)delegate { progressBar1.Value++; });
+            if (successCount >= progressBar1.Maximum)
+                progressBar1.BeginInvoke((MethodInvoker)delegate { Reset(); MessageBox.Show("全部已完成转换"); });
+
+        }
+
+        void DatToImage(FileInfo[] files, string xorKey)
+        {
+            IsRunning = true;
+            var key = Convert.ToInt64(xorKey, 16); ; //本机异或值
+            byte[] data = null;
+            foreach (var fileInfo in files)
+            {
+                if (IsStop) break;
+                if (fileInfo.Extension.ToLower().Contains("dat"))
+                {
+                    progressBar1.BeginInvoke((MethodInvoker)delegate { progressBar1.Value++; });
+                    using (var fs = new FileStream(fileInfo.FullName, FileMode.Open))
+                    {
+                        data = new byte[fs.Length];
+                        int count = fs.Read(data, 0, data.Length);
+                        var fileName = fileInfo.FullName + ".jpg";
+                        using (var nfs = new FileStream(fileName, FileMode.OpenOrCreate))
+                        {
+                            foreach (var b in data)
+                            {
+                                nfs.WriteByte((byte)(b ^ key));
+                                nfs.Flush();
+                            }
+                        }
+                        Console.WriteLine("正在处理：" + fileInfo.FullName);
+                    }
+                }
+                //break;
+            }
+            progressBar1.BeginInvoke((MethodInvoker)delegate { Reset(); MessageBox.Show("全部已完成转换"); });
+        }
+
+        void Reset()
+        {
+
+            IsRunning = false;
+            IsStop = false;
+            btnDatToImage.Enabled = true;
+            btnStop.Enabled = false;
+            progressBar1.Value = progressBar1.Maximum;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            txtFolder.Text = path;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            IsStop = true;
+        }
+    }
+}
